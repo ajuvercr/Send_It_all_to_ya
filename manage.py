0678311ed0480cfe7c641b9f029a29654ae6f519
ajuvercr <arthur.vercruysse@outlook.com>
2019-04-24 22:59:16 +0200

    add port

--- manage.py
+++ manage.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
     from django.core.management import execute_from_command_line
-
+    port = os.environ.get("PORT", 5000)
+    sys.argv[2] = sys.argv[2][:-4] + str(port)
     execute_from_command_line(sys.argv)
